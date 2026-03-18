7d83a9e04742aeb65aa7c41c67511b3059f31338
yaroslav-korol <18688588+yaroslav-korol@users.noreply.github.com>
2026-03-18 19:55:11 +0000

    feat(aws): Configure aws provider with localstack endpoints

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region                      = "us-east-1"
+  # LocalStack credentials
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  # LocalStack endpoints
+  endpoints {
+    cloudwatch = "http://localhost:4566"
+    ec2        = "http://localhost:4566"
+  }
+
+}
+
