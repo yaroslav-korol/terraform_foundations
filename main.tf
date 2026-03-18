089390dc54932c60a330c42b753cb9e8a2257e9e
yaroslav-korol <18688588+yaroslav-korol@users.noreply.github.com>
2026-03-18 21:16:40 +0000

    chore: Format terraform files with terraform fmt

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region                      = "us-east-1"
+  region = "us-east-1"
   # LocalStack credentials
   access_key                  = "test"
   secret_key                  = "test"
