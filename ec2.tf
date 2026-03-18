95e56168cbcca7309247259e60ab9f35a8930b03
yaroslav-korol <18688588+yaroslav-korol@users.noreply.github.com>
2026-03-18 19:55:50 +0000

    feat(aws): Define ec2 instance and ubuntu ami for localstack

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  # owners = ["099720109477"] # Canonical
+  owners = ["self"] # Fake owner for LocalStack compatibility / practice
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+
+}
