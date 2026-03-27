d7ba84dd5e6937fa2fb6925b7dc58ce17a2f7e4a
yaroslav-korol <18688588+yaroslav-korol@users.noreply.github.com>
2026-03-27 01:17:13 +0000

    feat(ec2): Attach ec2 instance to vpc subnet and security group

--- ec2.tf
+++ ec2.tf
@@ -14,6 +14,9 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
+
   tags = {
     Name = var.instance_name
   }
